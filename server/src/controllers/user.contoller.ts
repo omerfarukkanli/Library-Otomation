9af3572d86c1755fe279db4ac64c88849f3fd3b6
Omer Faruk Kanli <kanlomerfaruk@gmail.com>
2023-07-08 19:10:34 +0300

    Added user controller

---
+++ server/src/controllers/user.contoller.ts
@@ -0,0 +1,47 @@
+import User, { IUser } from "../models/user.model"
+import { Request, Response } from "express"
+
+export const registerUser = async (req: Request, res: Response) => {
+    const { name, lastname, password, email }: { name: string, lastname: string, password: string, email: string } = req.body;
+    try {
+        const userClone: IUser = await User.findOne({ email })
+        if (userClone) res.status(400).json({ message: "User already exists" })
+
+        if (!isValidEmail(email))
+            throw new Error('Geçersiz email formatı');
+
+        if (password.length < 8)
+            throw new Error('Şifre en az 8 karakter olmalıdır');
+        const username: string = `${name} ${lastname} `
+        const user: IUser = new User({
+            username,
+            password,
+            email,
+        });
+        const newUser = await user.save();
+        res.status(201).json(newUser);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
+
+export const loginUser = async (req: Request, res: Response) => {
+    try {
+        const { email, password }: { email: string, password: string } = req.body;
+        const user: IUser = await User.findOne({ email });
+        if (user) {
+            if (user.password != password) throw new Error('şifre yanlış');
+            else res.status(200).json(user);
+        }
+        else throw new Error('Böyle bir kullanıcı bulunamadı');
+
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+
+function isValidEmail(email: string): boolean {
+    const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+    return emailRegex.test(email);
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,15 @@
 import express from "express";
+import bodyParser from 'body-parser';
 import cors from "cors";
 import route from "./routes"
 import { connectToDB } from "./config/database"
+
 const App = express()
+
 App.use(cors());
+App.use(bodyParser.json());
+App.use(bodyParser.urlencoded({ extended: true }));
+
 connectToDB()
 App.use(route);
 
