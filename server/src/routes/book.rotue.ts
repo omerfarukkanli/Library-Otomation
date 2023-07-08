7363ac3178d0191f5870fa7b7e5ba03fea4b469a
Omer Faruk Kanli <kanlomerfaruk@gmail.com>
2023-07-08 18:13:29 +0300

    Added router form Book

--- server/src/routes/book.rotue.ts
+++ server/src/routes/book.rotue.ts
@@ -1,8 +1,13 @@
 import express from "express";
-import { getAllBooks } from '../controllers/book.contoller'
+import { getAllBooks, getBookById, getBookNameOrAuthor, deleteBook, upgradeBook } from '../controllers/book.contoller'
 
 const router = express.Router();
 
 router.get("/books", getAllBooks)
+router.get("/books:id", getBookById)
+router.get("/books/seach", getBookNameOrAuthor)
+router.get("/books/delete", deleteBook)
+router.get("/books/upgrade:id", upgradeBook)
+
 
 export default router
